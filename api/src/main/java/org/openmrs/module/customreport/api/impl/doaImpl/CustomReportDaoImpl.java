51b97c68a74b42c5e8b44d09e437ac2c16ba1249
akter_rakib <rakibccj@gmail.com>
2021-02-13 16:22:57 +0600

    Added a model named Team

--- api/src/main/java/org/openmrs/module/customreport/api/impl/doaImpl/CustomReportDaoImpl.java
+++ api/src/main/java/org/openmrs/module/customreport/api/impl/doaImpl/CustomReportDaoImpl.java
@@ -1,13 +1,19 @@
 package org.openmrs.module.customreport.api.impl.doaImpl;
 
+import org.openmrs.api.db.hibernate.DbSessionFactory;
 import org.openmrs.module.customreport.api.dao.CustomReportDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CustomReportDaoImpl implements CustomReportDao {
 	
+	@Autowired
+	DbSessionFactory dbSessionFactory;
+	
 	@Override
 	public String welCome() {
+		dbSessionFactory.getCurrentSession();
 		return "Alhamdulillah";
 	}
 }
